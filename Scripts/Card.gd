783bb6503a856320a5cf457fd6cfb4fe255c7a8f
Dylan <voonichu@gmail.com>
2024-06-19 19:30:38 -0400

    converted card script to implement a class

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -1,28 +1,28 @@
-class_name Card extends Node2D
-
+extends Node2D
+var Cards = preload("res://Scripts/cards.gd")
 @export var card_name: String = "Card Name"
-@export var card_description: String = "Card Description"
-@export var card_uses: int = 1
+@export var damage_amount: int = 5
+@export var element: String = "Fire"
+@export var durability: int = 1
+@export var cooldown: int = 3
+@export var discard: String = "More damage"
 @export var card_image: Node2D
 
-@onready var uses_lbl: Label = $DurabilityDisplay/DurabilityLbl
+@onready var durability_lbl: Label = $DurabilityDisplay/DurabilityLbl
 @onready var name_lbl: Label = $CardName/NameLbl
-@onready var description_lbl: Label = $CardDescription
+@onready var damage_lbl: Label = $CardDamage
+
 
 
 func _ready():
-	set_card_values(card_uses, card_name, card_description)
+	set_card_values(card_name, damage_amount, element, durability, cooldown, discard)
 	visible = false
 	
-func set_card_values(_uses: int, _name: String, _description: String):
-	card_name = _name
-	card_description = _description
-	card_uses = _uses
-	
-	
-	uses_lbl.set_text(str(_uses))
-	name_lbl.set_text(_name)
-	description_lbl.set_text(_description)
+func set_card_values(card_name: String, damage_amount: int, element: String, durability: int, cooldown: int, discard: String):
+	var card = Cards.new(card_name, damage_amount, element, durability, cooldown, discard)
+	durability_lbl.set_text(str(card.durability))
+	name_lbl.set_text(card.card_name)
+	damage_lbl.set_text(str(card.damage_amount))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
