7d09d5d3861e0a8d5328d9f0387eccd0a46bf158
Dylan <voonichu@gmail.com>
2024-06-19 21:50:34 -0400

    edited text files

--- Scripts/battle_scene.gd
+++ Scripts/battle_scene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var card_scene: PackedScene = preload("res://Scenes/card.tscn")
+@onready var card_scene = preload("res://Scenes/card.tscn")
 @onready var spawn_point = $Spawn
 
 func _ready():
@@ -13,16 +13,16 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	var card: Card = card_scene.instantiate()
+	var card = card_scene.instantiate()
 	spawn_point.add_child(card)
-	card.set_card_values(3, "cool card", "cool description")
+	card.set_card_values("cool card", 5, "water", 3, 9, "discard ability")
 	card.visible = true
 
 
 
 
 func _on_button_2_pressed():
-	var card: Card = card_scene.instantiate()
+	var card = card_scene.instantiate()
 	spawn_point.add_child(card)
-	card.set_card_values(5, "awesome card", "swag description")
+	card.set_card_values("awesome card", 8, "water", 1, 2, "discard damage")
 	card.visible = true
