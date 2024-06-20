7d7e4084eb75786b82d9d3548818b1d4f04a873b
Dylan <voonichu@gmail.com>
2024-06-19 21:47:59 -0400

    edited text files

--- Cards/CNLabel.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-func _ready():
-	var parent = get_parent()  
-	var card1 = parent.card1   
-	text = card1.card_name     
--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -1,5 +1,5 @@
 extends Node2D
-var Cards = preload("res://Scripts/cards.gd")
+
 @export var card_name: String = "Card Name"
 @export var damage_amount: int = 5
 @export var element: String = "Fire"
@@ -11,11 +11,11 @@ var Cards = preload("res://Scripts/cards.gd")
 @onready var durability_lbl: Label = $DurabilityDisplay/DurabilityLbl
 @onready var name_lbl: Label = $CardName/NameLbl
 @onready var damage_lbl: Label = $CardDamage
-
+@onready var Cards = preload("res://Scripts/cards.gd")
 
 
 func _ready():
-	set_card_values(card_name, damage_amount, element, durability, cooldown, discard)
+	#set_card_values(card_name, damage_amount, element, durability, cooldown, discard)
 	visible = false
 	
 func set_card_values(card_name: String, damage_amount: int, element: String, durability: int, cooldown: int, discard: String):
